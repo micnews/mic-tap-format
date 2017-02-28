d4c7286b98418c45cff217153bd835d38e92e6d4
Orry Baram <orrybaram@gmail.com>
2017-02-27 22:29:31 -0500

    adds bin

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const parser = require('../lib/parser');
+const formatAsSpec = require('../');
+
+const input = parser.observeStream(process.stdin);
+
+formatAsSpec(input).forEach(console.log.bind(console))
