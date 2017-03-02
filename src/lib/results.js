2e9758b559f870d18249a91fca24d22c5a1ce502
Orry Baram <orrybaram@gmail.com>
2017-03-01 23:49:52 -0500

    updates result styles

--- src/lib/results.js
+++ src/lib/results.js
@@ -9,44 +9,40 @@ export default (input$) => {
   const output$ = new Rx.Subject();
 
   // Output the results
-  input$.results$
-    .forEach(
-      (line) => {
-        switch (line.name) {
-          case 'tests': {
-            output$.onNext(pad(`${format.bgWhite.black(' ASSERTIONS ')}     ${format.white(line.count)}`));
-            break;
-          }
+  input$.results$.forEach((line) => {
+    switch (line.name) {
+      case 'tests': {
+        output$.onNext(pad(`${format.white('ASSERTIONS')}${format.dim('.....')}${format.white(line.count)}`));
+        break;
+      }
 
-          case 'pass': {
-            output$.onNext(pad(`${format.bgGreen.black(' PASSED ')}         ${format.green(line.count)}`));
-            break;
-          }
+      case 'pass': {
+        output$.onNext(pad(`${format.green('PASSED')}${format.dim('.........')}${format.green(line.count)}`));
+        break;
+      }
 
-          case 'fail': {
-            if (line.count > 0) {
-              output$.onNext(pad(`${format.bgRed.black(' FAILED ')}         ${format.red(line.count)}`));
-            }
-            break;
-          }
-          default:
-            output$.onNext('');
+      case 'fail': {
+        if (line.count > 0) {
+          output$.onNext(pad(`${format.red('FAILED')}${format.dim('.........')}${format.red(line.count)}`));
         }
-      },
-      () => {
-        output$.onNext('\n');
-        output$.onNext(
-          format.red.bold('PARSING ERROR:'),
-          'There was an error in the TAP parsing. Please open an issue at',
-          format.underline('https://github.com/scottcorgan/tap-out/issues.'),
-        );
-      },
-      () => {
-        output$.onNext('\n');
-        output$.onNext(pad(`${format.bgYellow.black(' DURATION ')}       ${format.yellow(prettyMs(timer()))}`));
-        output$.onNext('\n');
-      },
+        break;
+      }
+      default:
+        output$.onNext('');
+    }
+  },
+  () => {
+    output$.onNext('\n');
+    output$.onNext(
+      format.red.bold('PARSING ERROR:'),
+      'There was an error in the TAP parsing. Please open an issue at',
+      format.underline('https://github.com/scottcorgan/tap-out/issues.'),
     );
-
+  },
+  () => {
+    output$.onNext('\n');
+    output$.onNext(pad(`${format.yellow('DURATION')}${format.dim('.......')}${format.yellow(prettyMs(timer()))}`));
+    output$.onNext('\n');
+  });
   return output$;
 };
